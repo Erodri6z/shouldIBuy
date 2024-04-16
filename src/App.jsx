045be92de6315eabb68e4a0429498c3010ea6788
Erodri6z <erikrodriguez2417@gmail.com>
2024-04-16 13:50:01 -0500

    add an about page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/Landing/landing'
+import Footer from './components/footer/footer'
+import About from './pages/About/about'
 
 function App() {
 
@@ -13,7 +15,12 @@ function App() {
         path="/"
         element={<Landing />}
       />
+      <Route
+        path='/about'
+        element={<About />}
+      />
     </Routes>
+    <Footer />
     </>
   )
 }
---
+++ src/pages/About/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <a href="/">
+        <button>Return</button>
+      </a>
+      <h2></h2>
+
+    </div>
+  )
+}
+
+export default About
