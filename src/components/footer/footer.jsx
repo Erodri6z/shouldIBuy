426b9839506db9be711655c41f9fac3999880c8e
Erodri6z <erikrodriguez2417@gmail.com>
2024-04-16 13:43:57 -0500

    build out the footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="/about">
+        <h5>About</h5>
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
