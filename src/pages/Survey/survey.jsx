bbc952724bb362f61f459c7c32404153df724f77
Erodri6z <erikrodriguez2417@gmail.com>
2024-06-12 23:43:55 -0500

    add som ecss page to the survey

--- src/pages/Survey/survey.jsx
+++ src/pages/Survey/survey.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React from "react"
+import './survey.css'
+
 
 const Survey = () => {
   return (
     <div>
       <h1>Fill this out.</h1>
-      <div>
-        
+      <div className="">
+        <div className="chart-legend">
+          <h2>Question</h2>
+          <span></span>
+          <h2>Yes</h2>
+          <span></span>
+          <h2>No</h2>
+        </div>
       </div>
     </div>
   )
